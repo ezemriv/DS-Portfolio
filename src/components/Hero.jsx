572279ad76780b893cb0955e377f661e195ef4ed
ezemriv <ezequielmrivero@gmail.com>
2024-08-11 11:16:37 +0200

    v1 done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -121,13 +121,13 @@ const Hero = ({ name }) => {
               <SocialLinks />
             </div>
           </Col>
-          {/* <Col className="d-none d-md-block">
+          <Col className="d-none d-md-block">
             <img
               src={Logo}
               alt="React Logo"
               className="w-75 mx-auto hero-img"
             />
-          </Col> */}
+          </Col>
         </Row>
         <Row className="align-items-end down-container">
           <Col className="m-4 text-center">
